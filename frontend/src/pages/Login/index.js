d637b5fc59559070e32fe4f455087c4933875af0
Parmezani <fparmezani@hotmail.com>
2020-03-21 20:36:28 -0300

    Atualização

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,39 +1,39 @@
-import React, {useState} from 'react';
-import api from '../../services/api';
+import React, { useState } from "react";
+import api from "../../services/api";
 
-export default function Login({history}){
+export default function Login({ history }) {
+  const [email, setEmail] = useState("");
 
-    const [email, setEmail] = useState('')
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await api.post("/sessions", { email });
+    const { _id } = response.data;
+    localStorage.setItem("user", _id);
 
-    async function handleSubmit(event){
-      event.preventDefault();
-      const response = await api.post('/sessions', {email})
-      const {_id} = response.data;
-      localStorage.setItem('user', _id);
-        
-history.push('./DashBoard');
-
+    history.push("./DashBoard");
   }
 
-    return (
-        <React.Fragment>
-                    <p>
-        Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+  return (
+    <React.Fragment>
+      <p>
+        Ofereça <strong>spots</strong> para programadores e encontre{" "}
+        <strong>talentos</strong> para sua empresa
       </p>
 
-<form onSubmit={handleSubmit}>
-  <label htmlFor="email"></label>
-  <input 
-    type="email" 
-    id="email" 
-    placeholder="Seu melhor e-mail"
-    onChange={event => setEmail(event.target.value)}
-    value = {email}
-    >
-  </input>
-  <button type="submit" className="btn"> Entrar</button>
-</form>
-
-        </React.Fragment>
-    )
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email"></label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Seu melhor e-mail"
+          onChange={event => setEmail(event.target.value)}
+          value={email}
+        ></input>
+        <button type="submit" className="btn">
+          {" "}
+          Entrar
+        </button>
+      </form>
+    </React.Fragment>
+  );
 }
