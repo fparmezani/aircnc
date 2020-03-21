a0fecd954bda239b536761abd4882a08bebef8a8
Parmezani <fparmezani@hotmail.com>
2020-03-21 14:34:08 -0300

    Atualizacaoo de Cors

---
+++ frontend/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Login from './pages/Login';
+import DashBoard from './pages/DashBoard';
+import New from './pages/New';
+
+export default function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={Login} exact/>
+                <Route path="/dashboard" component={DashBoard}/>
+                <Route path="/new" component={New}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
